2d95a4550c48e837040b3b056597f035c9f7438c
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-09-02 19:13:52 +0200

    feat: update User with password and data types for columns

--- src/entities/users/user.entity.ts
+++ src/entities/users/user.entity.ts
@@ -1,10 +1,22 @@
-import { Table, Column, Model } from 'sequelize-typescript';
+import { Table, Column, Model, DataType } from 'sequelize-typescript';
 
 @Table
 export class User extends Model<User> {
-  @Column
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
   name: string;
 
-  @Column
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
   email: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  password: string;
 }
