1f41e5182ad48932a1a639e6824004acd7075fbb
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-09-02 23:04:34 +0200

    feat: add auth files

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { UserModule } from '../entities/users/user.module';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '24h' },
+    }),
+    forwardRef(() => UserModule),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService, JwtModule],
+})
+export class AuthModule {}
