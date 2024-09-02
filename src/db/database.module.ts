908e01282d77de3e9cec6b41aa9ed6dc59826986
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-09-02 18:53:31 +0200

    feat: update DatabaseModule with SequelizeModule

--- src/db/database.module.ts
+++ src/db/database.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { databaseProviders } from './database.providers';
 
 @Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
   providers: [...databaseProviders],
   exports: [...databaseProviders],
 })
