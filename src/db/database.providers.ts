baff33183271f9e5be0da53b2cc5f512e155eb97
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-09-02 18:52:30 +0200

    feat: add dotenv

--- src/db/database.providers.ts
+++ src/db/database.providers.ts
@@ -1,5 +1,8 @@
 import { Sequelize } from 'sequelize-typescript';
 import { User } from '../entities/users/user.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const databaseProviders = [
   {
