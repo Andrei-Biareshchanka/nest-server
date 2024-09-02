20ab6dfbee66f684d984639438ade78febe369ca
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-09-02 23:05:48 +0200

    feat: update user module

--- src/entities/users/user.module.ts
+++ src/entities/users/user.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { User } from './user.entity';
+import { AuthModule } from '../../auth/auth.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User])],
+  imports: [SequelizeModule.forFeature([User]), forwardRef(() => AuthModule)],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
