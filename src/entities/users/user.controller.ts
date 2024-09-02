3f70831f1ff8dd69b2d1414b34edf8ab2b4b80da
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-09-02 18:50:26 +0200

    feat: update user controller

--- src/entities/users/user.controller.ts
+++ src/entities/users/user.controller.ts
@@ -1,23 +1,22 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Get, Param, Body } from '@nestjs/common';
 import { UserService } from './user.service';
-import { User } from './user.entity';
+import { CreateUserDto } from './create-user.dto';
 
-@Controller('api/v1')
+const apiVersion = process.env.API_VERSION || 'v1';
+
+@Controller(`/api/${apiVersion}/users`)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('add-user')
-  async addUser(
-    @Body('name') name: string,
-    @Body('email') email: string,
-  ): Promise<User> {
-    const user = await this.userService.createUser(name, email);
-    console.log(user);
+  async addUser(@Body() createUserDto: CreateUserDto) {
+    const user = await this.userService.createUser(createUserDto);
+    console.log('User created:', user);
     return user;
   }
 
   @Get('get-user/:id')
-  async getUser(@Param('id') id: string): Promise<User> {
-    return this.userService.getUserById(+id);
+  async getUser(@Param('id') id: string) {
+    return this.userService.getUserById(Number(id));
   }
 }
