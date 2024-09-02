47e516c1957f8ff7b3e44df306d8372c3e1a6e7e
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-09-02 18:54:30 +0200

    feat: update AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './db/database.module';
-import { UserController } from './entities/users/user.controller';
-import { UserService } from './entities/users/user.service';
+import { UserModule } from './entities/users/user.module';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [UserController],
-  providers: [UserService],
+  imports: [DatabaseModule, UserModule],
 })
 export class AppModule {}
